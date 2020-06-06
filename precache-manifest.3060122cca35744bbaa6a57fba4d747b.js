0373635f159e2ab03ea9fb962382df7921d5ae50
PraneethNamala <praneeth.namala@credencys.com>
2020-06-06 13:25:39 +0530

    Updates

---
+++ precache-manifest.3060122cca35744bbaa6a57fba4d747b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b826276f2e996e5bc55d4873af50e1ec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b16842e0c78c8fad297d",
+    "url": "/static/css/main.d5bede02.chunk.css"
+  },
+  {
+    "revision": "898b448b49d035e1b3f7",
+    "url": "/static/js/2.81757e71.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.81757e71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b16842e0c78c8fad297d",
+    "url": "/static/js/main.3d7d28ba.chunk.js"
+  },
+  {
+    "revision": "5b2050e6027f2cdb8000",
+    "url": "/static/js/runtime-main.edb17ec9.js"
+  }
+]);
